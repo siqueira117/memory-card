7d1958e2cf9e999b5149c7c3f41fa025812eaf00
Erick Siqueira <erick.siqueira117@gmail.com>
2025-03-05 18:36:25 -0300

    fix: ajustando responsividade dos cards

--- resources/views/components/card-game.blade.php
+++ resources/views/components/card-game.blade.php
@@ -1,21 +1,24 @@
-@foreach ($games as $game)
-    <div class="col-md-3 mb-3">
-        <div class="card">
-            <img src="{{ $game['coverUrl'] }}" class="card-img-top" alt="{{ $game['name'] }}">
-            <div class="card-body">
-                <h5 class="card-title">{{ $game['name'] }}</h5>
-                <div class="dropdown">
-                    <a class="btn btn-custom my-3 dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                      Download
-                    </a>
-                  
-                    <ul class="dropdown-menu">
-                        @foreach ($platforms[$game['game_id']] as $platform)
-                            <li><a class="dropdown-item" href="{{ $platform['romUrl'] }}">{{$platform['platform_name']}}</a></li>
-                        @endforeach
-                    </ul>
+<div class="row g-3">
+    @foreach ($games as $game)
+        <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+            <div class="card h-100">
+                <img src="{{ $game['coverUrl'] }}" class="card-img-top" alt="{{ $game['name'] }}">
+                <div class="card-body d-flex flex-column">
+                    <h5 class="card-title">{{ $game['name'] }}</h5>
+                    <div class="mt-auto">
+                        <div class="dropdown">
+                            <a class="btn btn-custom my-3 dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Download
+                            </a>
+                            <ul class="dropdown-menu">
+                                @foreach ($platforms[$game['game_id']] as $platform)
+                                    <li><a class="dropdown-item" href="{{ $platform['romUrl'] }}">{{ $platform['platform_name'] }}</a></li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-@endforeach
+    @endforeach
+</div>
