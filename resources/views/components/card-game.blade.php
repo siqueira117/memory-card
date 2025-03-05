88c9e3c92316f6c8822351cf09242e146f827916
Erick Siqueira <erick.siqueira117@gmail.com>
2025-03-05 19:06:12 -0300

    fix: adicionando responsividade aos cards e add logo

--- resources/views/components/card-game.blade.php
+++ resources/views/components/card-game.blade.php
@@ -1,7 +1,7 @@
 <div class="row g-3">
     @foreach ($games as $game)
         <div class="col-6 col-sm-6 col-md-4 col-lg-3">
-            <div class="card h-100 w-75">
+            <div class="card h-100 w-75 ">
                 <img src="{{ $game['coverUrl'] }}" class="card-img-top" alt="{{ $game['name'] }}">
                 <div class="card-body d-flex flex-column">
                     <h5 class="card-title">{{ $game['name'] }}</h5>
