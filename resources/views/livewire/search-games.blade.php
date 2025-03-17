80c9a5bd2e3f8e8179a8844e4cfe913b9f6ca483
Erick Siqueira <erick.siqueira117@gmail.com>
2025-03-17 12:03:30 -0300

    feat: ajustando espaçamento

--- resources/views/livewire/search-games.blade.php
+++ resources/views/livewire/search-games.blade.php
@@ -1,42 +1,63 @@
 <div>
-    <div class="d-flex mb-3">
-        <input wire:model.live="search" type="text" id="search" class="search-bar w-100" aria-label="Small" aria-describedby="inputGroup-sizing-sm" placeholder="Pesquisar jogos...">
+    <div class="row mb-3">
+        @guest
+            <div class="col-12">
+                <input wire:model.live="search" type="text" id="search" class="search-bar w-100" aria-label="Small" aria-describedby="inputGroup-sizing-sm" placeholder="Pesquisar jogos..."> 
+            </div>
+        @endguest
         @auth
+            <div class="col-12 col-lg-8 mt-2">
+                <input wire:model.live="search" type="text" id="search" class="search-bar w-100" aria-label="Small" aria-describedby="inputGroup-sizing-sm" placeholder="Pesquisar jogos..."> 
+            </div>
             @if( Auth::user()->type === 'adm' )
-                <button class="btn btn-custom ms-2 w-20" data-bs-toggle="modal" data-bs-target="#gameModal">Add game</button>
-                <button class="btn btn-custom ms-2 w-20" data-bs-toggle="modal" data-bs-target="#gameManualModal">Add manual</button>
+                <div class="col-6 col-lg-2 mt-2">
+                    <button class="btn btn-custom h-100" data-bs-toggle="modal" data-bs-target="#gameModal">Add game</button>
+                </div>
+                <div class="col-6 col-lg-2 mt-2">
+                    <button ton class="btn btn-custom h-100" data-bs-toggle="modal" data-bs-target="#gameManualModal">Add manual</button>
+                </div>
             @endif
         @endauth
     </div>
     <!-- Filtros -->
-    <div class="d-flex mb-3">
-        <select class="me-2 form-select select-black" wire:model="genre">
-            <option value="" selected>Todos os gêneros</option>
-            @foreach($genres as $genre)
-                <option value="{{ $genre['genre_id'] }}">{{ $genre['name'] }}</option>
-            @endforeach
-        </select>
+    <div class="row mb-3">
+        <div class="col-6 col-lg-2 mt-2">
+            <select class="form-select select-black" wire:model="genre">
+                <option value="" selected>Todos os gêneros</option>
+                @foreach($genres as $genre)
+                    <option value="{{ $genre['genre_id'] }}">{{ $genre['name'] }}</option>
+                @endforeach
+            </select>
+        </div>
     
-        <select class="me-2 form-select select-black" wire:model="platform">
-            <option value="" selected>Todas as plataformas</option>
-            @foreach($platforms as $platform)
-                <option value="{{ $platform['platform_id'] }}">{{ $platform['name'] }}</option>
-            @endforeach
-        </select>
+        <div class="col-6 col-lg-2 mt-2">
+            <select class="form-select select-black" wire:model="platform">
+                <option value="" selected>Todas as plataformas</option>
+                @foreach($platforms as $platform)
+                    <option value="{{ $platform['platform_id'] }}">{{ $platform['name'] }}</option>
+                @endforeach
+            </select>
+        </div>
     
-        <select class="me-2 form-select select-black" wire:model="orderBy">
-            <option value="created_at" selected>Ordernar por Novidade</option>
-            <option value="name">Ordenar por Nome</option>
-            <option value="first_release_date">Ordenar por Data de Lançamento</option>
-        </select>
+        <div class="col-12 col-lg-2 mt-2">
+            <select class="form-select select-black" wire:model="orderBy">
+                <option value="created_at" selected>Ordernar por Novidade</option>
+                <option value="name">Ordenar por Nome</option>
+                <option value="first_release_date">Ordenar por Data de Lançamento</option>
+            </select>
+        </div>
     
-        <button class="btn btn-custom ms-2" wire:click="$toggle('orderDirection')">
-            {{ $orderDirection  ? '⬆️ Ascendente' : '⬇️ Descendente' }}
-        </button>
+        <div class="col-6 col-lg-3 mt-2">
+            <button class="btn btn-custom" wire:click="$toggle('orderDirection')">
+                {{ $orderDirection  ? '⬆️ Ascendente' : '⬇️ Descendente' }}
+            </button>
+        </div>
 
-        <button class="btn btn-custom ms-2" x-on:click="$wire.$refresh()">
-            Filtrar
-        </button>
+        <div class="col-6 col-lg-3 mt-2">
+            <button class="btn btn-custom" x-on:click="$wire.$refresh()">
+                Filtrar
+            </button>
+        </div>
     </div>
 
     <small>Total de jogos: {{ $allGames }}</small>
